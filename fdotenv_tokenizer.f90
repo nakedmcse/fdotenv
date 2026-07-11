b82d1b49ad6768323421992270a338c890066b12
Walker Aldridge <daldridge@lairdscomputer.com>
2026-07-11 11:05:49 -0500

    Tokenizer first draft

--- fdotenv_tokenizer.f90
+++ fdotenv_tokenizer.f90
@@ -2,6 +2,7 @@
 module fdotenv_tokenizer
     implicit none
     private
+    public :: fdotenv_next_token
 
     integer, parameter, public :: fdotenv_token_type_error   = -1
     integer, parameter, public :: fdotenv_token_type_eof     = 0
@@ -20,6 +21,37 @@ module fdotenv_tokenizer
     end type fdotenv_token_t
 
     contains
+        subroutine next_break(s, pos)
+            character(len=*), intent(in) :: s
+            integer, intent(inout) :: pos
+
+            do while (pos <= len(s) .and. .not. (s(pos:pos) == char(10) .or. s(pos:pos) == '='))
+                pos = pos + 1
+            end do
+        end subroutine next_break
+
+        subroutine next_given(s, pos, g, nl)
+            character(len=*), intent(in) :: s
+            character(len=1), intent(in) :: g
+            logical, intent(in) :: nl
+            integer, intent(inout) :: pos
+
+            do while (pos <= len(s) .and. .not. s(pos:pos) == g)
+                if (nl .and. s(pos:pos) == char(10)) exit
+                pos = pos + 1
+            end do
+        end subroutine next_newline
+
+        subroutine next_triple(s, pos, t)
+            character(len=*), intent(in) :: s
+            character(len=3), intent(in) :: t
+            integer, intent(inout) :: pos
+
+            do while (pos + 2 <= len(s) .and. .not. s(pos:pos+2) == t)
+                pos = pos + 1
+            end do
+        end subroutine next_triple
+
         subroutine fdotenv_next_token(s, pos, tok)
             character(len=*), intent(in) :: s
             integer :: pos, start
@@ -42,25 +74,58 @@ module fdotenv_tokenizer
                     return
                 case ('#')
                     tok%kind = fdotenv_token_type_hash
-                    ! find next newline or eof
-                    ! extract text to there for return
-                    ! move pos
+                    start = pos + 1
+                    call next_given(s,pos,char(10),.false.)
+                    tok%text = s(start:pos-1)
+                    return
+                case (char(10))
+                    tok%kind = fdotenv_token_type_newline
+                    tok%text = char(10)
+                    pos = pos + 1
                     return
                 case ('"')
+                    if (pos + 2 <= len(s) .and. s(pos:pos+2) == '"""') then
+                        tok%kind = fdotenv_token_type_double_quote_triple
+                        start = pos + 3
+                        pos = pos + 3
+                        call next_triple(s, pos, '"""')
+                        tok%text = s(start:pos)
+                        pos = pos + 3
+                        return
+                    end if
                     tok%kind = fdotenv_token_type_double_quote
-                    tok%text = '"'
+                    start = pos + 1
+                    pos = pos + 1
+                    call next_given(s,pos,'"',.true.)
+                    if (s(pos:pos) == char(10)) tok%kind = fdotenv_token_type_error
+                    tok%text = s(start:pos)
                     pos = pos + 1
                     return
                 case ("'")
+                    if (pos + 2 <= len(s) .and. s(pos:pos+2) == "'''") then
+                        tok%kind = fdotenv_token_type_single_quote_triple
+                        start = pos
+                        pos = pos + 3
+                        call next_triple(s, pos, "'''")
+                        tok%text = s(start:pos)
+                        pos = pos + 3
+                        return
+                    end if
                     tok%kind = fdotenv_token_type_single_quote
-                    tok%text = "'"
+                    start = pos + 1
+                    pos = pos + 1
+                    call next_given(s,pos,"'",.true.)
+                    if (s(pos:pos) == char(10)) tok%kind = fdotenv_token_type_error
+                    tok%text = s(start:pos)
                     pos = pos + 1
                     return
                 case default
-                    ! String token
+                    tok%kind = fdotenv_token_type_string
+                    start = pos
+                    call next_break(s,pos)
+                    tok%text = s(start:pos)
+                    return
             end select
-
         end subroutine fdotenv_next_token
 
-    ! TODO: Implement tokenizer
 end module fdotenv_tokenizer
